d9f7e8731930c9e6466197b6f234263d20fca1c7
RevanthDadi9 <dadirevanth9773@gmail.com>
2025-05-12 13:48:56 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.post('/upload', upload.single('file'), async (req, res) => {
       text = fs.readFileSync(filePath, 'utf8');
     }
 
-    const response = await axios.post('http://localhost:8000/analyze', {
+    const response = await axios.post('http://10.0.0.4:8000/analyze', {
       content: text
     });
 
